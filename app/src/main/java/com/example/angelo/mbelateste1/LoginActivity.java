4069f83c1df79561ff1bdee1d197d4ffb7fd7612
PosterApp <eltonvillas10@gmail.com>
2018-12-17 14:47:04 +0200

    Transicao de telas 50%

--- app/src/main/java/com/example/angelo/mbelateste1/LoginActivity.java
+++ app/src/main/java/com/example/angelo/mbelateste1/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.angelo.mbelateste1;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
@@ -38,6 +44,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private static final int STATE_VERIFY_SUCCESS = 4;
     private static final int STATE_SIGNIN_FAILED = 5;
     private static final int STATE_SIGNIN_SUCCESS = 6;
+    private static final int PHONE_NUMBER_RC = 20;
 
     // [START declare_auth]
     private FirebaseAuth mAuth;
@@ -61,6 +68,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button mVerifyButton;
     private Button mResendButton;
     private Button mSignOutButton;
+    private GoogleApiClient googleApiClient;
+    private SimpleCallback<String> phoneNumberCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -169,6 +178,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         };
         // [END phone_auth_callbacks]
+
+        requestPhoneNumber();
     }
 
     // [START on_start_check_user]
@@ -411,5 +422,43 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+
+    public void requestPhoneNumber(SimpleCallback<String> callback) {
+        phoneNumberCallback = callback;
+        HintRequest hintRequest = new HintRequest.Builder()
+                .setPhoneNumberIdentifierSupported(true)
+                .build();
+
+        PendingIntent intent = Auth.CredentialsApi.getHintPickerIntent(googleApiClient, hintRequest);
+        try {
+            startIntentSenderForResult(intent.getIntentSender(), PHONE_NUMBER_RC, null, 0, 0, 0);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Could not start hint picker Intent", e);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PHONE_NUMBER_RC) {
+            if (resultCode == RESULT_OK) {
+                Credential cred = data.getParcelableExtra(Credential.EXTRA_KEY);
+                if (phoneNumberCallback != null){
+                    phoneNumberCallback.onSuccess(cred.getId());
+                }
+            }
+            phoneNumberCallback = null;
+        }
+    }
+
+    private void requestPhoneNumber(){
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.CREDENTIALS_API)
+                .build();
+        requestPhoneNumber(result -> {
+            mPhoneNumberField.setText(result);
+        });
     }
 }
